ddc40f3422af6e49e56b021a8225224c2de5941b
Andrius Prieskienis <andrius.prieskienis@version1.com>
2026-02-25 10:27:19 -0500

    FindDoctorSearch updates

--- src/Components/FindDoctorSearch/FindDoctorSearch.js
+++ src/Components/FindDoctorSearch/FindDoctorSearch.js
@@ -14,28 +14,34 @@ const FindDoctorSearch = () => {
                     <div className='find-doctor-search'>
                         <h1>Find a Doctor</h1>
                         <div className="doctor-search-box">
-                            <input 
-                                type="text" 
-                                className='search-doctor-input-box'
-                                placeholder="Search for doctors, specialties, or locations..." 
-                                value={searchInput} 
-                                onChange={(e) => setSearchInput(e.target.value)} 
-                            />
-                            <button 
-                                className="search-btn" 
-                                onClick={() => setDoctorResultsVisible(true)}
-                            >
-                                Search
-                            </button>
+                            <div className='search-input-block'>
+                                <input 
+                                    type="text" 
+                                    className='search-doctor-input-box'
+                                    placeholder="Search for doctors, specialties, or locations..." 
+                                    value={searchInput} 
+                                    onChange={(e) => setSearchInput(e.target.value)} 
+                                    onFocus={() => setDoctorResultsVisible(true)}
+                                    onBlur={() => setDoctorResultsVisible(false)}
+                                />
+                                <button 
+                                    className="search-btn" 
+                                    onClick={() => setDoctorResultsVisible(true)}
+                                >
+                                    Search
+                                </button>
+                            </div>
+
+                            <div className="search-doctor-results" style={{ display: doctorResultsVisible ? 'block' : 'none' }}>
+                                {specialties.filter(specialty => specialty.toLowerCase().includes(searchInput.toLowerCase())).map((specialty, index) => (
+                                    <div key={index} className="search-doctor-result-item">
+                                        <h3>{specialty}</h3>
+                                        <p>Find top {specialty} in your area.</p>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
-                        <div className="doctor-results" style={{ display: doctorResultsVisible ? 'block' : 'none' }}>
-                            {specialties.filter(specialty => specialty.toLowerCase().includes(searchInput.toLowerCase())).map((specialty, index) => (
-                                <div key={index} className="doctor-result-item">
-                                    <h3>{specialty}</h3>
-                                    <p>Find top {specialty} in your area.</p>
-                                </div>
-                            ))}
-                        </div>
+                        
                     </div>
                 </div>
             </section>
