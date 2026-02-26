a7e1e91eff59d982fd89aed5f335621fcf30bcaa
Andrius Prieskienis <andrius.prieskienis@version1.com>
2026-02-26 08:45:15 -0500

    AppointmentForm

---
+++ src/Components/BookingConsultation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import DoctorCard from "./DoctorCard/DoctorCard";
+import FindDoctorSearch from "./FindDoctorSearch/FindDoctorSearch";
+
+const BookingConsultation = () => {
+    return (
+        <>
+            <FindDoctorSearch />
+            
+        </>
+    )
+}
+
+export default BookingConsultation;
