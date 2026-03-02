b4a640ea970ae1dda53682b840ec3117fa8aefdf
Andrius Prieskienis <andrius.prieskienis@version1.com>
2026-03-02 10:57:03 -0500

    Build and Deploy

--- server/index.js
+++ server/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const http = require('http');
 const connectToMongo = require('./db');
 const app = express();
-
+const path = require('path');
 
 app.set('view engine','ejs')
 app.use(express.static('public'))
@@ -20,6 +19,12 @@ connectToMongo();
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
